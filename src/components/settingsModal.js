615effa9ec1dcc36efd496fa1c095bb7bc57f768
Velavee <59717974+Velavee@users.noreply.github.com>
2021-07-09 16:51:42 -0700

    Added form to settings page

--- src/components/settingsModal.js
+++ src/components/settingsModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const SettingsModal = () => {
   const [show, setShow] = useState(false);
@@ -19,12 +20,31 @@ const SettingsModal = () => {
             <h4>FILTERS:</h4>
           </Modal.Header>
           <br />
-          <p>Placeholder</p>
-          <br />
-          <Modal.Header>
-            <h4>GAME SETTINGS:</h4>
-          </Modal.Header>
-          <p>Placeholder</p>
+          <Form>
+            <Form.Group>
+              <Form.Label>#hashtag or @handle</Form.Label>
+              <Form.Control type="text" defaultValue="@oregonstate" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>language</Form.Label>
+              <Form.Control as="select">
+                <option>en</option>
+              </Form.Control>
+            </Form.Group>
+            <br />
+            <Modal.Header>
+              <h4>GAME SETTINGS:</h4>
+            </Modal.Header>
+            <br />
+            <Form.Label>duration</Form.Label>
+            <Form.Control
+              type="text"
+              required
+              pattern="[0-9]{2}:[0-9]{2}"
+              defaultValue="1:00"
+            />
+          </Form>
+
           <br />
           <Modal.Header>
             <h4>STATS:</h4>

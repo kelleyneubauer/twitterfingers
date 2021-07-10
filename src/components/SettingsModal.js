77ca19ab35f50ee8ca9e7bccf736de2b0c6bbb4a
Velavee <59717974+Velavee@users.noreply.github.com>
2021-07-10 09:44:54 -0700

    Fixed browser warnings

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -9,23 +9,23 @@ const SettingsModal = () => {
   const handleShow = () => setShow(true);
 
   // language options at https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
-  let languages = [
-      {label: "English", value: "en"},
-      {label: "Arabic", value: "ar"},
-      {label: "German", value: "de"},
-      {label: "Russian", value: "ru"},
-      {label: "Spanish", value: "es"}
-    ]
+  let languages = [
+    { label: "English", value: "en" },
+    { label: "Arabic", value: "ar" },
+    { label: "German", value: "de" },
+    { label: "Russian", value: "ru" },
+    { label: "Spanish", value: "es" },
+  ];
 
   return (
     <>
-      <div class="nav-icon" onClick={handleShow}>
+      <div className="nav-icon" onClick={handleShow}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="40"
           height="40"
           fill="currentColor"
-          class="bi bi-gear-fill"
+          className="bi bi-gear-fill"
           viewBox="0 0 16 16"
         >
           <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
@@ -45,7 +45,11 @@ const SettingsModal = () => {
             <Form.Group>
               <Form.Label>language</Form.Label>
               <Form.Control as="select">
-                {languages.map((language) => <option key={language.label} value={language.value}>{language.label + " (" + language.value + ")"}</option>)}
+                {languages.map((language) => (
+                  <option key={language.label} value={language.value}>
+                    {language.label + " (" + language.value + ")"}
+                  </option>
+                ))}
               </Form.Control>
             </Form.Group>
             <br />

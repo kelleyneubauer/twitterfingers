3ff9323248c63a767d05c3a1d7cdeb55d902e9c8
Velavee <59717974+Velavee@users.noreply.github.com>
2021-07-10 21:38:37 -0700

    Defining mock data

--- src/App.js
+++ src/App.js
@@ -30,14 +30,6 @@ function App() {
               tweetDate={mockTweets[randomIdx].tweetDate}
               likes={mockTweets[randomIdx].likes}
               comments={mockTweets[randomIdx].comments}
-
-              // tweetImg="https://pbs.twimg.com/profile_images/856629049547935744/ScErYmP0_400x400.jpg"
-              // twitterName="Oregon State University"
-              // twitterHandle="OregonState"
-              // tweetDate="Jul 6"
-              // likes="2K"
-              // comments="20"
-              // tweetContent="What's that one thing you just can't let go? Beavers: Flowing Water"
             />
           </Col>
         </Row>

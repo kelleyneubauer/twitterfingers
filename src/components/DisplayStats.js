5bf625a96aa0f6081a181355b039d9e46d013687
Velavee <59717974+Velavee@users.noreply.github.com>
2021-07-11 09:38:51 -0700

    Got stats to display in UI

--- src/components/DisplayStats.js
+++ src/components/DisplayStats.js
@@ -2,14 +2,15 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { propTypes } from "react-bootstrap/esm/Image";
 
-const DisplayStats = () => {
+const DisplayStats = (props) => {
   return (
     <div className="stats-container">
       <Container>
         <Row>
-          <Col className="stats">27wpm</Col>
-          <Col className="stats">92%</Col>
+          <Col className="stats">{props.WPM}wpm</Col>
+          <Col className="stats">{props.accuracy}%</Col>
         </Row>
       </Container>
     </div>

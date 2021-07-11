f8f9ebe80c42c2d72872c8e94339696d2767cc27
Velavee <59717974+Velavee@users.noreply.github.com>
2021-07-10 21:31:34 -0700

    Loads in random mock tweet on refresh

---
+++ src/mockTweets.js
@@ -0,0 +1,48 @@
+const mockTweets = [
+  {
+    twitterHandle: "OregonState",
+    twitterName: "Oregon State University",
+    tweetImg:
+      "https://pbs.twimg.com/profile_images/856629049547935744/ScErYmP0_400x400.jpg",
+    tweetContent:
+      "Udderly ridiculous that a university public safety department would handle a call of loose cows? Then you might not have herd@OregonState is a land grant university. Cows mooved to a safe location. And we're done milking this tweet for cow puns",
+    tweetDate: "Jun 30",
+    likes: "64",
+    comments: "3",
+  },
+  {
+    twitterHandle: "OregonState",
+    twitterName: "Oregon State University",
+    tweetImg:
+      "https://pbs.twimg.com/profile_images/856629049547935744/ScErYmP0_400x400.jpg",
+    tweetContent:
+      "Congratulations to the 7,391 students graduating today. We are proud of your accomplishments, hard work, and dedication. As your next adventure unfolds remember that you'll always have a home here.",
+    tweetDate: "Jun 12",
+    likes: "252",
+    comments: "6",
+  },
+  {
+    twitterHandle: "OregonState",
+    twitterName: "Oregon State University",
+    tweetImg:
+      "https://pbs.twimg.com/profile_images/856629049547935744/ScErYmP0_400x400.jpg",
+    tweetContent:
+      "Fitting that on #WorldOceanDay we get to announce @oregonstate has been selected to lead a new NOAA institute for marine research, focusing on conservation, protection and restoration of marine resources and more in collaboration with @NOAAResearch: beave.es/3so.",
+    tweetDate: "Jun 8",
+    likes: "126",
+    comments: "0",
+  },
+  {
+    twitterHandle: "OregonState",
+    twitterName: "Oregon State University",
+    tweetImg:
+      "https://pbs.twimg.com/profile_images/856629049547935744/ScErYmP0_400x400.jpg",
+    tweetContent:
+      "What's that one thing you just can't let go? Beavers: Flowing Water",
+    tweetDate: "Jul 6",
+    likes: "2K",
+    comments: "20",
+  },
+];
+
+export default mockTweets;
